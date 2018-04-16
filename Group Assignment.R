b5269bf4176bb64b1f1ade6bf7945928561cc173
u143515 <38379846+u143515@users.noreply.github.com>
2018-04-16 23:29:51 +0200

    Update Group Assignment.R

--- Group Assignment.R
+++ Group Assignment.R
@@ -1,5 +1,13 @@
 # Loading libraries
   library(dplyr)
 
+# Loading data
+df <- read_csv("~/Desktop/agaricus-lepiota.data.txt", col_names = c("cap_shape", "cap_surface", "cap_color", "bruises", 
+                                     "odor", "gill_attachment", "gill_spacing", "gill_size", 
+                                     "gill_color", "stalk_shape", "stalk_root", "stalk_surface_above_ring", 
+                                     "stalk_surface_below_ring", "stalk_color_above_ring", "stalk_color_below_ring", 
+                                     "veil_type", "veil_color", "ring_number", "ring_type", "spore_print_color", "population", 
+                                     "habitat", ""))
+
   
 ### END OF CODE ###  
